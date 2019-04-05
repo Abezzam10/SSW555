6ccd6bcaddb77304100ef87021f5365f077d9cbe
Benjamin Cai <benjamincaiyh@gmail.com>
2019-04-05 14:53:00 -0400

    fit test16 with new test data

--- test/test_us16.py
+++ test/test_us16.py
@@ -4,14 +4,38 @@
 """
 import unittest
 from gedcom_ajry import Gedcom
+from mongo_db import MongoDB
 
 class test_us16(unittest.TestCase):
     """ Test cases for US16"""
 
-    def test_male_same_last_name_in_family(self):
-        """ male last names should be same in a family"""
-        ged = Gedcom('./GEDCOM_files/integrated_no_err.ged')
-        self.assertEqual(ged.us16_male_last_name(debug=True), [])
+    def test_same_lastname(self):
+        """ Positive test, males' last names are the same"""
+        mongo_instance = MongoDB()
+        mongo_instance.drop_collection("family")
+        mongo_instance.drop_collection("individual")
+
+        ged = Gedcom('./GEDCOM_files/us16/us16_male_last_name_same.ged')
+        ged.insert_to_mongo()
+        
+        self.assertEqual(
+            ged.us16_male_last_name(debug=True),
+            []
+        )
+
+    def test_diff_lastname(self):
+        """ Negative test, males' last names are different"""
+        mongo_instance = MongoDB()
+        mongo_instance.drop_collection("family")
+        mongo_instance.drop_collection("individual")
+
+        ged = Gedcom('./GEDCOM_files/us16/us16_male_last_name_diff.ged')
+        ged.insert_to_mongo()
+
+        self.assertEqual(
+            ged.us16_male_last_name(debug=True),
+            [('@F1@', '@I2@, @I3@', 'LastName,Test')]
+        )
 
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
